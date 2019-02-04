bdc641f5ba28068d259fd4236ce3bb2d6aaf3c6f
Lars Folkestad <lcfolkes@stud.ntnu.no>
2019-02-04 08:59:55 +0100

    merged remote changes to local

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+// @ts-ignore
+@Component( {
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+} )
+export class ServerComponent{
+  serverID: number = 10;
+  serverStatus: string = 'offline';
+
+  getServerStatus(){
+    return this.serverStatus;
+  }
+
+}
